b301ac9c9dc3176ea3d9f45861af5300733ed126
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-07-04 13:51:26 -0500

    Avatar image updated

--- services/src/posts.rs
+++ services/src/posts.rs
@@ -10,17 +10,17 @@ pub struct PostFile {
 pub static POSTS: [PostFile; 4] = [
     PostFile {
         content: include_str!("../../posts/build_blog.md"),
-        modified_time: 1654609915763,
+        modified_time: 1656959820000,
         filename: "build_blog",
     },
     PostFile {
         content: include_str!("../../posts/add_links.md"),
-        modified_time: 1654855511389,
+        modified_time: 1656959820000,
         filename: "add_links",
     },
     PostFile {
         content: include_str!("../../posts/project_test.md"),
-        modified_time: 1654855511389,
+        modified_time: 1656959820000,
         filename: "project_test",
     },
     PostFile {
